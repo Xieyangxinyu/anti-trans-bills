604f517206e6c65bf54860552565197314b76a48
Yangxinyu <xinyux@wharton.upenn.edu>
2024-03-17 17:25:43 -0400

    topic

--- topic_modelling.py
+++ topic_modelling.py
@@ -11,7 +11,7 @@ import os
 import pickle
 import dill
 
-def get_topic_model(bill_type: str = 'Healthcare'):
+def get_topic_model(df, bill_type: str = 'Healthcare'):
     # check if 'data/{bill_type} exists
     if os.path.exists(f'data/{bill_type}'):
         docs = pickle.load(open(f'data/{bill_type}/docs.pkl', 'rb'))
@@ -21,17 +21,15 @@ def get_topic_model(bill_type: str = 'Healthcare'):
         info_tab = pd.read_csv(f'data/{bill_type}/topic_info.csv')
     else:
         os.makedirs(f'data/{bill_type}')
-
-        df = pd.read_csv('data/bills_18_23.csv')
         docs = df[df['Bill Type'] == bill_type]['Bill Description']
-        sentences = [sent_tokenize(doc) for doc in docs]
-        docs = [sentence for doc in sentences for sentence in doc]
+        #sentences = [sent_tokenize(doc) for doc in docs]
+        #docs = [sentence for doc in sentences for sentence in doc]
         docs = pd.Series(docs).unique()
         
         embedding_model = SentenceTransformer("all-mpnet-base-v2")
         embeddings = embedding_model.encode(docs, show_progress_bar=True)
-        umap_model = UMAP(n_neighbors=min(15, int(len(docs)/5)), n_components=10, min_dist=0.0, metric='cosine', random_state=42)
-        hdbscan_model = HDBSCAN(min_cluster_size=3, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
+        umap_model = UMAP(n_neighbors=min(15, int(len(docs)/5)), n_components=5, min_dist=0.1, metric='cosine', random_state=42)
+        hdbscan_model = HDBSCAN(min_cluster_size=5, metric='euclidean', cluster_selection_method='eom', prediction_data=True)
         vectorizer_model = CountVectorizer(stop_words="english", min_df=2, ngram_range=(1, 2))
         keybert_model = KeyBERTInspired()
         pos_model = PartOfSpeech("en_core_web_sm")
@@ -39,17 +37,16 @@ def get_topic_model(bill_type: str = 'Healthcare'):
 
         # GPT-3.5
 
-        client = openai.OpenAI(api_key="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")
+        client = openai.OpenAI(api_key="sk-*******")
 
         prompt = """
         I have a topic that contains the following documents:
         [DOCUMENTS]
-        The topic is described by the following keywords: [KEYWORDS]
 
-        Based on the information above, extract a short but highly descriptive topic label of at most 5 words. Make sure it is in the following format:
+        Based on the information above, extract an informative topic label of at most 10 words. Stick to the original language of the document as much as possible and avoid mentioning of locations. Make sure it is in the following format:
         topic: <topic label>
         """
-        openai_model = OpenAI(client, model="gpt-3.5-turbo-1106", exponential_backoff=True, chat=True, prompt=prompt)
+        openai_model = OpenAI(client, model="gpt-4-0125-preview", exponential_backoff=True, chat=True, prompt=prompt)
 
         # All representation models
         representation_model = {
